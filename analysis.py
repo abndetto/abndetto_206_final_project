a23276829b1b5b17ce9dbc46fc85bab196dd0e30
Antonio Benedetto <abndetto@umich.edu>
2023-12-08 18:54:26 -0500

    third visual

--- analysis.py
+++ analysis.py
@@ -38,7 +38,7 @@ def get_rotten_tomatoes_data_score_over_90(cur):
 # movie title, local id, and year made to a list of tuples. The function returns the list of tuples.
 def get_rapid_api_data(cur):
     return_list = []
-    cur.execute('SELECT Rapid_API.id, Rapid_API.rated, Rapid_API.released, Rapid_API.runtime, Genres.genre, Rapid_API.country, Rapid_API.awards, Rapid_API.boxoffice, Rapid_API.imdbRating, Rapid_API.metascore FROM Rapid_API JOIN Genres ON Rapid_API.genre = Genres.id')
+    cur.execute('SELECT Rapid_API.id, Rapid_API.rated, Rapid_API.released, Rapid_API.runtime, Genres.genre, MovieCountries.country, Rapid_API.awards, Rapid_API.boxoffice, Rapid_API.imdbRating, Rapid_API.metascore FROM Rapid_API JOIN Genres ON Rapid_API.genre = Genres.id JOIN MovieCountries ON Rapid_API.country = MovieCountries.id')
     for rval in cur:
         rx = (rval[0], rval[1], rval[2], rval[3], rval[4], rval[5], rval[6], rval[7], rval[8], rval[9])
         return_list.append(rx)
@@ -61,10 +61,11 @@ rapi_metascore = [movie[9] for movie in rapi_data]
 rapi_data = {'ID': rapi_id, 'Rated': rapi_rated, 'Released': rapi_released, 'Runtime': rapi_runtime, 'Genre': rapi_genre, 'Country': rapi_country, 'Awards': rapi_awards, 'Box Office': rapi_boxoffice, 'IMDB Rating': rapi_imdbRating, 'Metascore': rapi_metascore}
 rapi_df = pd.DataFrame(rapi_data)
 rapi_df['Metascore'] = pd.to_numeric(rapi_df['Metascore'], errors='coerce')
+
+
+# Graph Average Metascore by Genre
 average_metascore_by_genre = rapi_df.groupby('Genre')['Metascore'].mean().reset_index()
 average_metascore_by_genre['Metascore'] = average_metascore_by_genre['Metascore'].round(2)
-
-# Plot the bar graph
 plt.figure(figsize=(10, 6))
 plt.bar(average_metascore_by_genre['Genre'], average_metascore_by_genre['Metascore'], color='skyblue')
 plt.xlabel('Genre')
@@ -72,6 +73,20 @@ plt.ylabel('Average Metascore')
 plt.title('Average Metascore by Genre')
 plt.xticks(rotation=45, ha='right')
 for i, value in enumerate(average_metascore_by_genre['Metascore']):
+    plt.text(i, value + 0.1, str(value), ha='center', va='bottom')
+plt.tight_layout()
+plt.show()
+
+# Graph Average Metascore by Country
+average_metascore_by_country = rapi_df.groupby('Country')['Metascore'].mean().reset_index()
+average_metascore_by_country['Metascore'] = average_metascore_by_country['Metascore'].round(2)
+plt.figure(figsize=(10, 6))
+plt.bar(average_metascore_by_country['Country'], average_metascore_by_country['Metascore'], color='green')
+plt.xlabel('Country')
+plt.ylabel('Average Metascore')
+plt.title('Average Metascore by Country')
+plt.xticks(rotation=45, ha='right')
+for i, value in enumerate(average_metascore_by_country['Metascore']):
     plt.text(i, value + 0.1, str(value), ha='center', va='bottom')
 plt.tight_layout()
 plt.show()
